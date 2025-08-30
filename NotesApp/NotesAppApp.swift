a11ea3292de31250c81e443f33266e34cf88ac08
Muhammad Nouman <hammer.class82@gmail.com>
2025-08-30 12:29:24 +0500

    Initial Commit

---
+++ NotesApp/NotesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesAppApp.swift
+//  NotesApp
+//
+//  Created by MNouman on 30/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct NotesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
